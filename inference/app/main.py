6b80f116fad2113ce61bb0f65b68115c9a6476c7
jackvial <vialjack@gmail.com>
2021-03-20 22:11:34 -0400

    Format and filter results.

--- inference/app/main.py
+++ inference/app/main.py
@@ -21,7 +21,8 @@ qa_model = AutoModelForQuestionAnswering.from_pretrained("/models/distilbert-bas
 
 
 class ModelInput(BaseModel):
-    text: str
+    contexts: List[str]
+    query: str
 
 
 # @app.post("/predict")
@@ -33,25 +34,30 @@ class ModelInput(BaseModel):
 #     }
 #     return res
  
-@app.get("/predict")
-async def predict():
-    text = r"""
-    🤗 Transformers (formerly known as pytorch-transformers and pytorch-pretrained-bert) provides general-purpose
-    architectures (BERT, GPT-2, RoBERTa, XLM, DistilBert, XLNet…) for Natural Language Understanding (NLU) and Natural
-    Language Generation (NLG) with over 32+ pretrained models in 100+ languages and deep interoperability between
-    TensorFlow 2.0 and PyTorch.
-    """
+@app.post("/predict")
+async def predict(model_input: ModelInput):
+    # text = r"""
+    # 🤗 Transformers (formerly known as pytorch-transformers and pytorch-pretrained-bert) provides general-purpose
+    # architectures (BERT, GPT-2, RoBERTa, XLM, DistilBert, XLNet…) for Natural Language Understanding (NLU) and Natural
+    # Language Generation (NLG) with over 32+ pretrained models in 100+ languages and deep interoperability between
+    # TensorFlow 2.0 and PyTorch.
+    # """
+    # text = model_input.contexts
 
-    questions = [
-        "How many pretrained models are available in Transformers?",
-        "What does Transformers provide?",
-        "Transformers provides interoperability between which frameworks?",
-    ]
+    # questions = [
+    #     "How many pretrained models are available in Transformers?",
+    #     "What does Transformers provide?",
+    #     "Transformers provides interoperability between which frameworks?",
+    # ]
+    
+    question = model_input.query
 
+    scores = []
     answers = []
     answer_starts = []
     answer_ends = []
-    for question in questions:
+    results = []
+    for text in model_input.contexts:
         inputs = qa_tokenizer.encode_plus(question, text, add_special_tokens=True, return_tensors="pt")
         input_ids = inputs["input_ids"].tolist()[0]
 
@@ -61,25 +67,30 @@ async def predict():
         # print("answer_start_scores: ", answer_start_scores)
         # print("answer_end_scores: ", answer_end_scores)
 
+        # Get the most likely beginning of answer with the argmax of the score
         answer_start = torch.argmax(
             answer_start_scores
-        )  # Get the most likely beginning of answer with the argmax of the score
-        print("answer_start: ", answer_start)
+        )
         answer_starts.append(answer_start.item())
-        answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
+        
+        # Get the most likely end of answer with the argmax of the score
+        answer_end = torch.argmax(answer_end_scores) + 1
         answer_ends.append(answer_end.item())
 
         answer = qa_tokenizer.convert_tokens_to_string(qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-
-        # print(f"Question: {question}")
-        # print(f"Answer: {answer}\n")
-        answers.append(answer)
+        
+        results.append({
+            "answer": answer,
+            "score": torch.sigmoid(torch.max(
+                answer_start_scores
+            )).item(),
+            "context": text,
+            "answer_start": answer_start.item(),
+            "answer_end": answer_end.item(),
+        })
     
     
     res = {
-        "questions": questions,
-        "answers": answers,
-        "answer_starts": answer_starts,
-        "answer_ends": answer_ends,
+        "results": results
     }
     return res
