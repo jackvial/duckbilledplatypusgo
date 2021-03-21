e46035139f6c5e038a509e663dccd912027c55a3
jackvial <vialjack@gmail.com>
2021-03-21 13:01:49 -0400

    Refactored inference service.

--- inference/app/main.py
+++ inference/app/main.py
@@ -1,7 +1,6 @@
 import os
 import json
 import torch
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from typing import List
 from fastapi import (
     FastAPI,
@@ -10,87 +9,60 @@ from fastapi import (
     HTTPException,
 )
 from pydantic import BaseModel
+import functools
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
 app = FastAPI()
 
 # @see https://huggingface.co/transformers/v2.8.0/usage.html
-qa_tokenizer = AutoTokenizer.from_pretrained("/models/distilbert-base-uncased-distilled-squad")
-# print("qa_tokenizer: ", qa_tokenizer)
-qa_model = AutoModelForQuestionAnswering.from_pretrained("/models/distilbert-base-uncased-distilled-squad")
-# print("qa_model: ", qa_model)
+# There are currently no docs for new versions but it's much the same.
+qa_tokenizer = AutoTokenizer.from_pretrained(
+    "/models/distilbert-base-uncased-distilled-squad"
+)
+qa_model = AutoModelForQuestionAnswering.from_pretrained(
+    "/models/distilbert-base-uncased-distilled-squad"
+)
 
 
 class ModelInput(BaseModel):
     contexts: List[str]
-    query: str
+    question: str
 
 
-# @app.post("/predict")
-# async def predict(model_input: ModelInput):
-#     res = {
-#         "model_input": model_input,
-#         "label": "POSITIVE",
-#         "score": 0.51,
-#     }
-#     return res
- 
+def run_qa_inference(context, question):
+    inputs = qa_tokenizer.encode_plus(
+        question, context, add_special_tokens=True, return_tensors="pt"
+    )
+    input_ids = inputs["input_ids"].tolist()[0]
+    text_tokens = qa_tokenizer.convert_ids_to_tokens(input_ids)
+
+    # @see https://stackoverflow.com/questions/64901831/huggingface-transformer-model-returns-string-instead-of-logits
+    answer_start_preds, answer_end_preds = qa_model(**inputs).values()
+
+    # Get the most likely beginning and end of answer with the argmax of the predictions
+    answer_start = torch.argmax(answer_start_preds)
+    answer_end = torch.argmax(answer_end_preds) + 1
+    answer = qa_tokenizer.convert_tokens_to_string(
+        qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end])
+    )
+
+    return {
+        "inputs": inputs,
+        "answer": answer,
+        "score": torch.sigmoid(torch.max(answer_start_preds)).item(),
+        "context": context,
+        "answer_start": answer_start.item(),
+        "answer_end": answer_end.item(),
+    }
+
+
 @app.post("/predict")
 async def predict(model_input: ModelInput):
-    # text = r"""
-    # 🤗 Transformers (formerly known as pytorch-transformers and pytorch-pretrained-bert) provides general-purpose
-    # architectures (BERT, GPT-2, RoBERTa, XLM, DistilBert, XLNet…) for Natural Language Understanding (NLU) and Natural
-    # Language Generation (NLG) with over 32+ pretrained models in 100+ languages and deep interoperability between
-    # TensorFlow 2.0 and PyTorch.
-    # """
-    # text = model_input.contexts
-
-    # questions = [
-    #     "How many pretrained models are available in Transformers?",
-    #     "What does Transformers provide?",
-    #     "Transformers provides interoperability between which frameworks?",
-    # ]
-    
-    question = model_input.query
-
-    scores = []
-    answers = []
-    answer_starts = []
-    answer_ends = []
-    results = []
-    for text in model_input.contexts:
-        inputs = qa_tokenizer.encode_plus(question, text, add_special_tokens=True, return_tensors="pt")
-        input_ids = inputs["input_ids"].tolist()[0]
-
-        text_tokens = qa_tokenizer.convert_ids_to_tokens(input_ids)
-        # https://stackoverflow.com/questions/64901831/huggingface-transformer-model-returns-string-instead-of-logits
-        answer_start_scores, answer_end_scores = qa_model(**inputs).values()
-        # print("answer_start_scores: ", answer_start_scores)
-        # print("answer_end_scores: ", answer_end_scores)
-
-        # Get the most likely beginning of answer with the argmax of the score
-        answer_start = torch.argmax(
-            answer_start_scores
+    return {
+        "results": list(
+            map(
+                functools.partial(run_qa_inference, question=model_input.question),
+                model_input.contexts,
+            )
         )
-        answer_starts.append(answer_start.item())
-        
-        # Get the most likely end of answer with the argmax of the score
-        answer_end = torch.argmax(answer_end_scores) + 1
-        answer_ends.append(answer_end.item())
-
-        answer = qa_tokenizer.convert_tokens_to_string(qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-        
-        results.append({
-            "answer": answer,
-            "score": torch.sigmoid(torch.max(
-                answer_start_scores
-            )).item(),
-            "context": text,
-            "answer_start": answer_start.item(),
-            "answer_end": answer_end.item(),
-        })
-    
-    
-    res = {
-        "results": results
     }
-    return res
--- web/app/server.js
+++ web/app/server.js
@@ -1,34 +1,32 @@
 const dotenv = require('dotenv');
 dotenv.config();
-// const httpClient = require('http');
 const superagent = require('superagent');
 const express = require('express');
 const app = express();
 const dbConnectionPool = require('./database/connection');
-
 app.use(express.static('public'));
 
 app.get('/search', (searchReq, searchRes) => {
-  const query = searchReq.query.q;
+  const question = searchReq.query.q;
   dbConnectionPool.query(
     //'SELECT id, body FROM articles WHERE body LIKE ?'
     'SELECT id, body FROM articles WHERE id > 0',
-    ['%' + query + '%'],
-    (err, rows, fields) => {
+    ['%' + question + '%'],
+    (err, rows) => {
       if (err) throw err;
 
       superagent
         .post('http://platypus_inference/predict')
-        .send({ query: query, contexts: rows.map((item) => item.body) }) // sends a JSON post body
+        .send({ question: question, contexts: rows.map((item) => item.body) })
         .set('accept', 'json')
         .end((err, predictRes) => {
-          // console.log('superagent err: ', err);
-          // console.log('superagent res: ', JSON.stringify(res.body));
           searchRes.status(200).json({
-            results: predictRes.body.results.sort((a, b) => {
-              return b.score - a.score;
-            }).filter(item => item.score > 0.5),
-            query,
+            results: predictRes.body.results
+              .sort((a, b) => {
+                return b.score - a.score;
+              })
+              .filter((item) => item.score > 0.5),
+            question,
           });
         });
     }
