01ba5da0ed83a6700667866bfc6cbab4eda9b578
jackvial <vialjack@gmail.com>
2021-03-28 15:36:23 -0400

    Cleanup debug code.

--- inference/app/main.py
+++ inference/app/main.py
@@ -31,65 +31,11 @@ class ModelInput(BaseModel):
     question: str
 
 
-def span_to_answer(tokenizer, text: str, start: int, end: int):
-    """
-    When decoding from token probabilities, this method maps token indexes to actual word in the initial context.
-
-    Args:
-        text (:obj:`str`): The actual context to extract the answer from.
-        start (:obj:`int`): The answer starting token index.
-        end (:obj:`int`): The answer end token index.
-
-    Returns:
-        Dictionary like :obj:`{'answer': str, 'start': int, 'end': int}`
-    """
-    words = []
-    token_idx = char_start_idx = char_end_idx = chars_idx = 0
-
-    for i, word in enumerate(text.split(" ")):
-        token = tokenizer.tokenize(word)
-
-        # Append words if they are in the span
-        if start <= token_idx <= end:
-            if token_idx == start:
-                char_start_idx = chars_idx
-
-            if token_idx == end:
-                char_end_idx = chars_idx + len(word)
-
-            words += [word]
-
-        # Stop if we went over the end of the answer
-        if token_idx > end:
-            break
-
-        # Append the subtokenization length to the running index
-        token_idx += len(token)
-        chars_idx += len(word) + 1
-
-    # Join text with spaces
-    return {
-        "answer": " ".join(words),
-        "start": max(0, char_start_idx),
-        "end": min(len(text), char_end_idx),
-    }
-
-
 def run_qa_inference(context, question):
     inputs = qa_tokenizer.encode_plus(
         question, context, add_special_tokens=True, return_tensors="pt"
     )
     input_ids = inputs["input_ids"].tolist()[0]
-    # print("input_ids: ", input_ids)
-    text_tokens = qa_tokenizer.convert_ids_to_tokens(input_ids)
-    print("text_tokens: ", text_tokens)
-    print("qa_tokenizer.convert_tokens_to_string(text_tokens): ", qa_tokenizer.convert_tokens_to_string(text_tokens))
-    print("join text tokens: ", " ".join(text_tokens).replace(" ##", "").strip())
-    
-
-    # print("len(context.split()): ", len(context.split(" ")))
-    # print("len(input_ids): ", len(input_ids))
-    # print("len(text_tokens): ", len(text_tokens))
 
     # @see https://stackoverflow.com/questions/64901831/huggingface-transformer-model-returns-string-instead-of-logits
     answer_start_preds, answer_end_preds = qa_model(**inputs).values()
@@ -98,15 +44,9 @@ def run_qa_inference(context, question):
     answer_start = torch.argmax(answer_start_preds)
     answer_end = torch.argmax(answer_end_preds) + 1
 
-    # print("ids to tokens: ", qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-
     answer = qa_tokenizer.convert_tokens_to_string(
         qa_tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end])
     )
-    # print("qa_tokenizer.words_to_chars(input_ids[answer_start:answer_end]): ", qa_tokenizer.words_to_chars(input_ids[answer_start:answer_end]))
-    # print("input_ids[answer_start]: ", input_ids[answer_start])
-    # print("input_ids[answer_end]: ", input_ids[answer_end])
-    # print("span_to_answer: ", span_to_answer(qa_tokenizer, context, answer_start, answer_end))
 
     return {
         "answer": answer,
