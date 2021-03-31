77e92e65e4913c8ebf53e8a5dacb143bceddbdf7
jackvial <vialjack@gmail.com>
2021-03-31 19:38:05 -0400

    Add search limit with env vars. Increase model results threshold.

--- web/app/app/Http/Controllers/SearchController.php
+++ web/app/app/Http/Controllers/SearchController.php
@@ -11,11 +11,12 @@ class SearchController extends Controller
 {
     public function search(Request $request)
     {
-        $contexts = collect(DB::select(DB::raw("SELECT * from articles WHERE MATCH(body) AGAINST (?);"), [
-            $request->q
+        $contexts = collect(DB::select(DB::raw("SELECT * from articles WHERE MATCH(body) AGAINST (?) LIMIT ?;"), [
+            $request->q,
+            env('SEARCH_LIMIT', 10),
         ]))->map(function($item) {
             return $item->body;
-        });
+        })->take(env('TAKE_N_RESULTS', 5));
         $response = Http::post(env('QA_INFERENCE_HOST') . '/predict', [
             'question' => $request->q,
             'contexts' => $contexts,
